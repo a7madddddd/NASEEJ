37db2e85953cb22570f8e625d0677a7fc96278e2
a7madddddd <142698638+a7madddddd@users.noreply.github.com>
2024-11-06 18:49:36 +0300

    add db

---
+++ Naseej_Project/Models/Request.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Naseej_Project.Models;
+
+public partial class Request
+{
+    public int RequestId { get; set; }
+
+    public int? UserId { get; set; }
+
+    public int? ServiceId { get; set; }
+
+    public DateTime? RequestDate { get; set; }
+
+    public virtual Service? Service { get; set; }
+
+    public virtual User? User { get; set; }
+}
--- Naseej_Project/Program.cs
+++ Naseej_Project/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,20 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+// Configure CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+
 
 var app = builder.Build();
 
@@ -16,8 +32,17 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
+
+// Enable CORS before authentication/authorization
+app.UseCors("AllowAllOrigins");
+
 app.UseAuthorization();
 
 app.MapControllers();
-
 app.Run();
+
+
+
+
+
