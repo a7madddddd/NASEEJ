6bf1977052035d7e31fdc0580f542770b3d1778e
mustafa-almomani <almomani363@gmail.com>
2024-11-08 16:35:12 +0300

    2

--- Naseej_Project/Models/User.cs
+++ Naseej_Project/Models/User.cs
@@ -27,6 +27,8 @@ public partial class User
 
     public string? PasswordSalt { get; set; }
 
+    public string Email { get; set; } = null!;
+
     public virtual ICollection<Request> Requests { get; set; } = new List<Request>();
 
     public virtual ICollection<Testimonial> Testimonials { get; set; } = new List<Testimonial>();
