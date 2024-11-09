1a59a1c7be6dc9922ed4c2cfa2083a8c4a9327e6
yousef khraiwesh <yu.yousseff@gmail.com>
2024-11-09 18:18:22 +0300

    add Testimonials

---
+++ Naseej_Project/Controllers/TestimonialsController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Naseej_Project.DTOs;
+using Naseej_Project.Models;
+
+namespace Naseej_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestimonialsController : ControllerBase
+    {
+
+        private readonly MyDbContext _db;
+
+        public TestimonialsController(MyDbContext db)
+        {
+            _db = db;
+        }
+
+
+        //////////////////////////////////////////////////////
+
+
+        [HttpGet("GetAllTestimonials")]
+        public IActionResult GetAllTestimonial()
+        {
+            var GetAllTestimonials = _db.Testimonials.ToList();
+
+            return Ok(GetAllTestimonials);
+        }
+
+        //////////////////////////////////////////////////////
+
+
+        [HttpGet("GetTestimonial/{id}")]
+        public IActionResult GetTestimonial(int id)
+        {
+            var Testimonialmessage = _db.Testimonials.FirstOrDefault(m => m.UserId == id);
+
+            if (Testimonialmessage == null)
+            {
+                return NotFound(new { Message = "Message not found" });
+            }
+
+            var messageDto = new TestimonialDTO
+            {
+                Email = Testimonialmessage.Email,
+                Firstname = Testimonialmessage.Firstname,
+                Lastname = Testimonialmessage.Lastname,
+                TheTestimonials = Testimonialmessage.TheTestimonials,
+
+            };
+
+            return Ok(Testimonialmessage);
+        }
+
+        //////////////////////////////////////////////////////
+
+        [HttpPost("PostTestimonial")]
+        public IActionResult PostMessage([FromForm] TestimonialDTO TestimonialsDto)
+        {
+
+
+            var Testimonials = new Testimonial
+            {
+                Firstname = TestimonialsDto.Firstname,
+                Lastname = TestimonialsDto.Lastname,
+                Email = TestimonialsDto.Email,
+                TheTestimonials = TestimonialsDto.TheTestimonials,
+
+            };
+
+            _db.Testimonials.Add(Testimonials);
+            _db.SaveChanges();
+
+            return Ok(new { Message = " message sent successfully !" });
+        }
+
+
+    }
+}
